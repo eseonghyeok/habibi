b05bf159e179fb6ba6f535497096df923229ebd5
seonghyeok <seonghyeok@pentasecurity.com>
2024-04-20 17:35:07 +0900

    [fix] footer 멘트 변경

--- my-app/src/components/views/homePage/footer.js
+++ my-app/src/components/views/homePage/footer.js
@@ -17,8 +17,8 @@ function Footer() {
   return (
     <footer style={footerStyle}>
       <p style={{ fontSize: '10px' }}>
-        ● 하비비FC는 열정과 친목을 바탕으로 활동하는 풋살 동호회입니다.<br/>
-        ● 연고지: 영등포구 (영등포공원 풋살경기장)<br/>
+        사이트 기능 관련 건의사항 및 개선의견 환영합니다.<br/>
+        ● hometown: 영등포구 (영등포공원 풋살경기장)<br/>
         ● instagram: @habibi.footballclub<br/><br/> 
         회장: 신종윤  /  운영진: 정회화, 김주환, 김진호  /  개발자: 이성혁 (eseonghyeok@naver.com)
       </p>
