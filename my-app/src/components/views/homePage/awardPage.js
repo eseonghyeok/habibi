3be9be2896c9e8e8cc6c3fa9f39359d02cdc5d3a
seonghyeok <seonghyeok@pentasecurity.com>
2024-05-24 18:20:05 +0900

    [feat] 임시 명예의 전당 4월까지 게시

--- my-app/src/components/views/homePage/awardPage.js
+++ my-app/src/components/views/homePage/awardPage.js
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Jan from './award/Jan.png';
+import Feb from './award/Feb.png';
+import Mar from './award/Mar.png';
+import Apr from './award/Apr.png';
 
 function HallOfFamePage() {
-    // 명예의 전당에 전시할 이미지 URL 배열
-    const imageUrls = [
-        'https://example.com/image1.jpg',
-        'https://example.com/image2.jpg',
-        'https://example.com/image3.jpg',
-        // 필요한 만큼 이미지 URL을 추가합니다.
-    ];
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+    const imageUrls = [Jan, Feb, Mar, Apr];
+    const monthNames = ['1월', '2월', '3월', '4월'];
+
+    const nextSlide = () => {
+        setCurrentImageIndex((prevIndex) => (prevIndex === imageUrls.length - 1 ? 0 : prevIndex + 1));
+    };
+
+    const prevSlide = () => {
+        setCurrentImageIndex((prevIndex) => (prevIndex === 0 ? imageUrls.length - 1 : prevIndex - 1));
+    };
 
     return (
-        <div style={{ textAlign: 'center' }}>
-            <h1>명예의 전당</h1>
-            <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
-                {imageUrls.map((imageUrl, index) => (
-                    <div key={index} style={{ margin: '10px' }}>
-                        <img src={imageUrl} alt={`Hall of Fame ${index + 1}`} style={{ maxWidth: '300px', maxHeight: '300px' }} />
+        <div style={{ textAlign: 'center', minHeight: '600px' }}>
+            <h1 style={{ marginBottom: "10px", color: "black", fontSize: "30px" }}>🗽명예의 전당🗽</h1>
+            <div>
+                <h1 style={{ marginBottom: "10px", color: "black", fontSize: "15px" }}>월간 MVP</h1>
+                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                    <button onClick={prevSlide} style={{ marginRight: '10px' }}>&#10094;</button>
+                    <div style={{ position: 'relative' }}>
+                        <div>{monthNames[currentImageIndex]}</div>
+                        <img src={imageUrls[currentImageIndex]} alt={`Month MVP ${currentImageIndex + 1}`} style={{ maxWidth: '300px', maxHeight: '300px' }} />
                     </div>
-                ))}
-            </div>
+                    <button onClick={nextSlide} style={{ marginLeft: '10px' }}>&#10095;</button>
+                </div>
+            </div>  
         </div>
     );
-};
+}
 
 export default HallOfFamePage;
