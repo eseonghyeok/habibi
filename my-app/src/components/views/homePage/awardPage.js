6496b01abe50f3f5530f3d9b78639abcf2b6636c
seonghyeok <seonghyeok@pentasecurity.com>
2024-06-05 18:32:27 +0900

    [fix] 명예의 전당 앨범 개선

--- my-app/src/components/views/homePage/awardPage.js
+++ my-app/src/components/views/homePage/awardPage.js
@@ -1,14 +1,16 @@
 import React, { useState } from 'react';
-import Jan from './award/Jan.png';
-import Feb from './award/Feb.png';
-import Mar from './award/Mar.png';
-import Apr from './award/Apr.png';
+import awardpage from './awardpage.jpg'
+import Jan from './award/player/jan-juhwan.png';
+import Feb1 from './award/player/feb-hoihwa.png';
+import Feb2 from './award/player/feb-juhwan.png';
+import Mar from './award/player/mar-hoihwa.png';
+import Apr from './award/player/apr-juhwan.png';
 
 function HallOfFamePage() {
     const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-    const imageUrls = [Jan, Feb, Mar, Apr];
-    const monthNames = ['1월', '2월', '3월', '4월'];
+    const imageUrls = [Jan, Feb1, Feb2, Mar, Apr];
+    const monthNames = ['2024 January', '2024 February 공동', '2024 February 공동', '2024 March', '2024 April'];
 
     const nextSlide = () => {
         setCurrentImageIndex((prevIndex) => (prevIndex === imageUrls.length - 1 ? 0 : prevIndex + 1));
@@ -18,18 +20,49 @@ function HallOfFamePage() {
         setCurrentImageIndex((prevIndex) => (prevIndex === 0 ? imageUrls.length - 1 : prevIndex - 1));
     };
 
+    const arrowButtonStyle = {
+        marginRight: '10px',
+        marginLeft: '10px',
+        background: 'none',
+        border: 'none',
+        fontSize: '35px',
+        color: 'white',
+        cursor: 'pointer',
+        transition: 'transform 0.3s ease',
+    };
+
+    const arrowButtonHoverStyle = {
+        ...arrowButtonStyle,
+        transform: 'scale(1.2)',
+    };
+
     return (
-        <div style={{ textAlign: 'center', minHeight: '600px' }}>
-            <h1 style={{ marginBottom: "10px", color: "black", fontSize: "30px" }}>🗽명예의 전당🗽</h1>
+        <div style={{ textAlign: 'center', minHeight: "100vh", padding: "10px", backgroundImage: `url(${awardpage})`, backgroundSize: "cover", backgroundPosition: "center" }}>
+            <h1 style={{ marginBottom: "70px", color: "white", fontSize: "30px" }}>🗽HABIBI 명예의 전당🗽</h1>
             <div>
-                <h1 style={{ marginBottom: "10px", color: "black", fontSize: "15px" }}>월간 MVP</h1>
+                <h1 style={{ marginBottom: "-30px", color: "white", fontSize: "20px" }}>HABIBI FOOTBALL CLUB</h1>
+                <h1 style={{ marginBottom: "-10px", color: "white", fontSize: "30px" }}>PLAYER OF THE MONTH</h1>
                 <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                    <button onClick={prevSlide} style={{ marginRight: '10px' }}>&#10094;</button>
+                    <button
+                        style={arrowButtonStyle}
+                        onMouseOver={(e) => e.currentTarget.style.transform = 'scale(1.2)'}
+                        onMouseOut={(e) => e.currentTarget.style.transform = 'scale(1)'}
+                        onClick={prevSlide}
+                    >
+                        &#10094;
+                    </button>
                     <div style={{ position: 'relative' }}>
-                        <div>{monthNames[currentImageIndex]}</div>
-                        <img src={imageUrls[currentImageIndex]} alt={`Month MVP ${currentImageIndex + 1}`} style={{ maxWidth: '300px', maxHeight: '300px' }} />
+                        <h1 style={{ fontSize: '25px', marginBottom: '25px', color: 'white' }}>{monthNames[currentImageIndex]}</h1>
+                        <img style={{ maxHeight: '400px' }} src={imageUrls[currentImageIndex]} alt={`Month MVP ${currentImageIndex + 1}`}/>
                     </div>
-                    <button onClick={nextSlide} style={{ marginLeft: '10px' }}>&#10095;</button>
+                    <button
+                        style={arrowButtonStyle}
+                        onMouseOver={(e) => e.currentTarget.style.transform = 'scale(1.2)'}
+                        onMouseOut={(e) => e.currentTarget.style.transform = 'scale(1)'}
+                        onClick={nextSlide}
+                    >
+                        &#10095;
+                    </button>
                 </div>
             </div>  
         </div>
