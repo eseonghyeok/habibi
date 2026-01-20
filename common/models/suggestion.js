9ebdcfd4041f89eba284c503aecaa6e80a2e85bb
gtjeong <gtjeong@pentasecurity.com>
2026-01-20 11:41:45 +0900

    [fix] 건의사항 작성할 때 작성자도 기재하도록 변경
     건의사항 작성할 때 작성자도 기재하도록 변경
      - 작성자는 로그인한 경우에만 보임

--- common/models/suggestion.js
+++ common/models/suggestion.js
@@ -9,7 +9,7 @@ module.exports = function (sequelize, DataTypes) {
       defaultValue: DataTypes.UUIDV4
     },
     content: {
-      type: DataTypes.TEXT,
+      type: DataTypes.JSON,
       allowNull: false
     },
     check: {
