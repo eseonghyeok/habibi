f9851811552b115c7d995a3ea620894755a540aa
gtjeong <gtjeong@pentasecurity.com>
2026-02-26 18:22:29 +0900

    [test] 테스트 콘솔 로그 제거
     테스트 콘솔 로그 제거

--- my-app/src/components/views/homePage/suggestionPage.js
+++ my-app/src/components/views/homePage/suggestionPage.js
@@ -74,9 +74,7 @@ function SuggestionPage() {
   };
 
   const formatDate = (dateString) => {
-    console.log(dateString)
     const date = new Date(dateString);
-    console.log(date)
     return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')} ${String(date.getHours()).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}`;
   };
 
